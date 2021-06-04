efc8ac0f9ae38a260838fd7cc3c2c03696386af9
Pavel Yanovsky <humgate@gmail.com>
2021-06-04 20:02:49 +0300

    updated some comments

--- src/main/java/DoublelinkedList.java
+++ src/main/java/DoublelinkedList.java
@@ -4,7 +4,6 @@ public class DoublelinkedList<E> {
      * @param <E>  - element type
      */
 
-    // Nested inner class
     static class Node<E> {
         E item; // the data
         Node<E> next; //link to next
@@ -20,26 +19,29 @@ public class DoublelinkedList<E> {
     private Node<E> last;
 
     public DoublelinkedList() {
-        //Both @first & @last are just links to first and last DoublelinkedList elements. They are not the elements
-        //themselves. So they really need to be just links to Node <E>. But initialization of @first and @last
-        //as Node <E> allows more simple, better readable and compact code in methods implementations
-        //So init them as two Nodes first->last
-        first = new Node<E>(null, null, null);
-        last = new Node<E>(first, null, null);
+
+        /* Both @first & @last are just links to first and last DoublelinkedList elements. They are not the elements
+         *themselves. So they really need to be just links to Node <E>. But initialization of @first and @last
+         *as Node <E> allows more simple, better readable and compact code in methods implementations
+         * So init them as two Nodes first->last
+         */
+
+        first = new Node<>(null, null, null);
+        last = new Node<>(first, null, null);
         first.next = last;
     }
 
-    //add new elements to the end, mark it last
+    //adds new element to the end, marks it as last
     public void addToEnd(E val) {
         last.prev.next = last.prev = new Node<>(last.prev, val, last);
     }
 
-    //add to begin, mark it first
+    //adds new element to begin, marks it as first
     public void addToBegin(E val) {
         first.next.prev = first.next = new Node<>(first, val, first.next);
     }
 
-    //get element located at idx
+    //gets element located at idx
     public E get(int idx) {
         Node<E> current = first;
         for (int i = -1; i < idx; i++) {
@@ -48,7 +50,7 @@ public class DoublelinkedList<E> {
         return current.item;
     }
 
-    //set element value to element located at idx
+    //sets element value for element located at idx
     public void set(int idx, E val) {
         Node<E> current = first;
         for (int i = -1; i < idx; i++) {
@@ -57,29 +59,29 @@ public class DoublelinkedList<E> {
         current.item = val;
     }
 
-    //remove from end, mark previous to removed as last
+    //removes last element from the end, marks previous to removed one (new last) as last
     public void removeFromEnd() {
         last.prev = last.prev.prev;
         last.prev.next = last;
     }
 
-    //remove from  begin, mark next to removed as first
+    //removes first element from begin, marks next to removed one (new first) as first
     public void removeFromBegin() {
         first.next = first.next.next;
         first.next.prev = first;
     }
 
-    //get last. @last is just the link to real last, so return previous to @last
+    //gets last. @last is just the link to real last, so returns previous to @last
     public E getLast() {
         return last.prev.item;
     }
 
-    //get first. @first is just the link to real last, so return next to @first
+    //gets first. @first is just the link to real last, so returns next to @first
     public E front() {
         return first.next.item;
     }
 
-    //if @first and last link to each other, then no real elements between them in the list
+    //if @first and @last link to each other, then no real elements between them in the list
     public boolean isEmpty() {
         return (first.next == last && last.prev == first);
     }
