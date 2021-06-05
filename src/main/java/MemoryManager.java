cba94704aac492812bb4d5fa6d1511e2471b3e88
Pavel Yanovsky <humgate@gmail.com>
2021-06-05 23:13:04 +0300

    Continued implementation - 5.

--- src/main/java/MemoryManager.java
+++ src/main/java/MemoryManager.java
@@ -7,12 +7,11 @@ import java.util.Objects;
 @Getter@Setter
 public class MemoryManager {
     /*
-     * Memory Space Item is structure consisting of three fields: index, length, status. Is used to store info about
-     * a memory piece.
-     * index - number of memory space item
+     * Memory Space Item is structure used to store info about a memory piece.
+     * index -  number of memory space item
      * length - length of memory space item
      * status - indicates whether space item is allocated or free
-     * node - the link to
+     *
      */
 
     private class MemorySpaceItem {
@@ -44,38 +43,33 @@ public class MemoryManager {
     /*
      * HasMap stores pairs of: memory item index - memory item length. Used to check if the given index has
      * allocated memory item starting at that index.
-     * (It is possible to seek for that index right in the double linked
-     * list. But in this case it will iterate through all dlist elements each time (worst case). To speed up this
-     * search we store each allocated memspace index along with its length in the HashMap)
+     * (It is possible to seek for that index right in the double linked dlist. But in this case it will iterate
+     * through all dlist elements each time (worst case). To speed up this search we store each allocated mem space
+     * index along with its length in the HashMap)
      *
      */
     HashMap<Integer, Integer> allocMemMap = new HashMap<>();
 
 
     MemoryManager(int size){
-        //our memory size
         this.size = size;
 
         //initialize manager by adding one free MemorySpaceItem with starting index = 0 and size equals to @size
         dlist.addToBegin(new MemorySpaceItem(0,size,false));
-
     }
 
     public int malloc (int n) {
         /*
-         * Space item (MemorySpaceItem object) is the node with three fields - status (free/allocated), start index,
-         * length.
          * 1. Find Least Recently Used (least recently freed) memory space item in stack (stack.item)
-         * 2. check if n is less or equal to stack.item.length returned at step above. If greater - error
-         * 3. find free dlist.item (MemorySpaceItem object) corresponding to this stack.item. Delete this stack.item
-         * from stack.
-         * 4. add new allocated dlist.item, set i from free dlist.item, n (from @param) to the dlist. Put new pair int
-         * allocated memory hash map (allocMemMap), with key = i and value = dlist.item
-         * 5. calculate new free space item, which will be free dlist.item excluding from it the allocated dlist.item
-         * 6. delete free dlist.item
-         * 7. add new free (reduced) dlist.item
-         * 8. add new stack.item corresponding to dlist.item
-         * 9. return allocated dlist.item.index (from step 4)
+         * 2. Check if n is less or equal to stack.item.length returned at step above. If greater - error
+         * 3. Find free dlist.item (MemorySpaceItem object) corresponding to this (1) stack.item. Delete this
+         * stack.item from stack.
+         * 4. Mark this (3) free dlist.item as allocated, change its n (to @param). Put new pair into allocated memory
+         * hash map (allocMemMap), with key = i and value = n.
+         * 5. Calculate new free space item, left from previous item (3) after allocation new item into it.
+         * 6. Add new free (reduced) dlist.item next to this (4) just allocated item.
+         * 8. Add new stack.item corresponding to new free (6) dlist.item
+         * 9. return allocated dlist.item.index (from 4)
          *
          *
          *
@@ -86,15 +80,15 @@ public class MemoryManager {
     public int free (int i) {
         /*
          * 1. Find allocated space.item with index = i in allocMemMap.
-         * If none return -1 , else proceed below
-         * 2. As soon as it exists, find it in the dlist. Set its status to free
-         * 3. Find appropriate stack.item and delete it
+         * If none return -1, else proceed below
+         * 2. As soon as it exists, find it in the dlist. Set its status to free in the dlist
          * 3. Check if there are adjacent free memory space items to this one in dlist using dlist getPrev/getNext.
          * If yes:
          * merge them by setting leftmost (closest to dlist begin) item length equal to sum of all
          * found free adjacent item lengths. Delete all found adjacent neighbour dlist.items to the right
          * (closer to the end of dlist). Along with deleting these dlist.items, find corresponding ones in the stack
          *  and delete them as well
+         * 4. Add new free stack.item corresponding to the item got on previous step
          *
          */
 
