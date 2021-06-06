39cd584f371a515ebf7697a7f4c49d5e58950f9b
Pavel Yanovsky <humgate@gmail.com>
2021-06-07 00:48:09 +0300

    malloc(n) - updated test

--- src/test/java/MallocTest.java
+++ src/test/java/MallocTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class MallocTest {
     /**
      * Tests for class DoublelinkedList.
@@ -10,9 +12,26 @@ public class MallocTest {
         manager.malloc(5);
         manager.malloc(2);
 
-        System.out.println(manager.dlist.get(0).index + " " + manager.dlist.get(0).length + " " + manager.dlist.get(0).allocated);
-        System.out.println(manager.dlist.get(1).index + " " + manager.dlist.get(1).length +  " " + manager.dlist.get(0).allocated);
-        System.out.println(manager.dlist.get(2).index + " " + manager.dlist.get(2).length +  " " + manager.dlist.get(0).allocated);
+        System.out.println(
+                manager.dlist.get(0).index + " " + manager.dlist.get(0).length + " " + manager.dlist.get(0).allocated);
+        System.out.println(
+                manager.dlist.get(1).index + " " + manager.dlist.get(1).length +  " " + manager.dlist.get(1).allocated);
+        System.out.println(
+                manager.dlist.get(2).index + " " + manager.dlist.get(2).length +  " " + manager.dlist.get(2).allocated);
+
+        assertEquals(manager.dlist.get(0).index,0);
+        assertEquals(manager.dlist.get(0).length,5);
+        assertTrue(manager.dlist.get(0).allocated);
+
+        assertEquals(manager.dlist.get(1).index,5);
+        assertEquals(manager.dlist.get(1).length,2);
+        assertTrue(manager.dlist.get(1).allocated);
+
+        assertEquals(manager.dlist.get(2).index,7);
+        assertEquals(manager.dlist.get(2).length,93);
+        assertFalse(manager.dlist.get(2).allocated);
+
+        assertNull(manager.dlist.get(3));
 
     }
 }
