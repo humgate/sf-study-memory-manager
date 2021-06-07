6dd458920c52417003caed6842d1b21e5cebe7a3
Pavel Yanovsky <humgate@gmail.com>
2021-06-07 18:53:59 +0300

    Passed malloc(n) tests. Some free(i) tests still fail

--- src/test/java/MallocTest.java
+++ src/test/java/MallocTest.java
@@ -4,18 +4,27 @@ import static org.junit.Assert.*;
 
 public class MallocTest {
     /**
-     * Tests for class DoublelinkedList.
+     * Tests for malloc(n).
      */
     @Test
-    public final void test2() {
+    public final void test1() {
 
         MemoryManager manager = new MemoryManager(100);
 
+        //malloc 4 items
         manager.malloc(5);
         manager.malloc(2);
+        manager.malloc(4);
+        manager.malloc(10);
 
-        assertEquals(manager.malloc(94),-1);
-
+        /*
+         *As result must have items index-length-allocated
+         * 0: 0-5-true
+         * 1: 5-2-true
+         * 2: 7-4-true
+         * 3: 11-21-true
+         * 4: 21-100-false
+         */
         assertEquals(manager.dlist.getMemItemByIndex(0).index,0);
         assertEquals(manager.dlist.getMemItemByIndex(0).length ,5);
         assertTrue(manager.dlist.getMemItemByIndex(0).allocated);
@@ -25,9 +34,17 @@ public class MallocTest {
         assertTrue(manager.dlist.getMemItemByIndex(1).allocated);
 
         assertEquals(manager.dlist.getMemItemByIndex(2).index,7);
-        assertEquals(manager.dlist.getMemItemByIndex(2).length ,93);
-        assertFalse(manager.dlist.getMemItemByIndex(2).allocated);
+        assertEquals(manager.dlist.getMemItemByIndex(2).length ,4);
+        assertTrue(manager.dlist.getMemItemByIndex(2).allocated);
 
-        assertNull(manager.dlist.getMemItemByIndex(3));
+        assertEquals(manager.dlist.getMemItemByIndex(3).index,11);
+        assertEquals(manager.dlist.getMemItemByIndex(3).length ,10);
+        assertTrue(manager.dlist.getMemItemByIndex(3).allocated);
+
+        assertEquals(manager.dlist.getMemItemByIndex(4).index,21);
+        assertEquals(manager.dlist.getMemItemByIndex(4).length ,79);
+        assertFalse(manager.dlist.getMemItemByIndex(4).allocated);
+
+        assertNull(manager.dlist.getMemItemByIndex(5));
     }
 }
